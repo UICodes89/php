f73d7be27375a3cf1dfb181b8e77719722d6d8d4
manoj <manojkshukla87@gmail.com>
2014-10-03 17:22:57 -0700

    second commit

--- db.php
+++ db.php
@@ -1,4 +1,18 @@
 <?php
-echo"database code";
+public class connection{
+ public function __construct(){
+    try{
+	   $connect=mysql_connect('localhost', 'root', '');
+	   mysql_select_db("user");
+	}catch($e){
+	
+	  echo "new exception occur".$e;
+	}
+    
+	
+ }
+
+}
+$connect=new connection();
 
 ?>
